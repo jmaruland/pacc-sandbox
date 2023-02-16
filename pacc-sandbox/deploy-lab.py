2fbb8e77160b86dcba6e7738ccd9ac329b5780db
Juan Marulanda <jmaruland@bnl.gov>
2023-02-16 10:14:15 -0500

    Create deploy-lab.py
    Add deploy file at the same level of run file

---
+++ pacc-sandbox/deploy-lab.py
@@ -0,0 +1,14 @@
+from flows import pipe
+from prefect.deployments import Deployment
+from prefect.filesystems import GitHub
+
+github_block = GitHub.load("prefect-github-sandbox")
+
+deploy_gh = Deployment.build_from_flow(
+    flow=pipe,
+    name="GH Python Deploy",
+    storage=github_block
+)
+
+if __name__ == "__main__":
+    deploy_gh.apply()
